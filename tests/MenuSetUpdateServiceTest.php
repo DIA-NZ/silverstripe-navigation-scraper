c5ec5851a51dc8c2d72a08772d7067297ac215b3
James Goodman <james.s.goodman@gmail.com>
2015-12-10 15:07:20 +1300

    Add some assertions to menu set update test

--- tests/MenuSetUpdateServiceTest.php
+++ tests/MenuSetUpdateServiceTest.php
@@ -9,6 +9,7 @@ class MenuSetUpdateServiceTest extends SapphireTest {
 	}
 
 	public function testSomething() {
+		Config::inst()->remove('NavigationScraper', 'MenuSets');
 		Config::inst()->update('NavigationScraper', 'MenuSets', array(
 			'Test' => array(
 				'PageToScrape' => 'https://www.example.com',
@@ -17,8 +18,17 @@ class MenuSetUpdateServiceTest extends SapphireTest {
 		));
 
 		$updateService = new MenuSetUpdateService();
-
 		$updateService->scrapeConfiguredMenuSets();
+
+		$menuItems = MenuItem::get();
+
+		$this->assertEquals(1, count($menuItems));
+
+		$menuItem = $menuItems->first();
+
+		$this->assertEquals('Test', $menuItem->MenuSet);
+		$this->assertEquals('Hello world!', $menuItem->LinkText);
+		$this->assertEquals('https://www.example.com/hello-world', $menuItem->LinkHref);
 	}
 
 }
